0ff6f8462ea975c77fdd81bf805aeb6d1dc95bc3
Valerii Petriuk <vpetruk257@gmail.com>
2024-05-27 16:44:22 +0300

    add models user

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    #my app
+    "base",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_ROOT = 'static/images/'
+
+AUTH_USER_MODEL = 'base.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ backend/base/models/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    class RolesChoice(models.TextChoices):
+        STUDENT = "Student", "Student"
+        TEACHER = "Teacher", "Teacher"
+
+    surname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11, unique=True)
+    groups = models.CharField(max_length=10)
+    role = models.CharField(choices=RolesChoice.choices, default=RolesChoice.STUDENT, max_length=10)
+    photo = models.ImageField(upload_to='user/photo', blank=True, null=True)
+
