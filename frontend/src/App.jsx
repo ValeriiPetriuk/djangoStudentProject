ce895f7b97fd798bf181df4f8f8acf779540a31a
Valerii Petriuk <vpetruk257@gmail.com>
2024-06-05 18:57:16 +0300

    start frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+    return (
+    <>
+      <div>
+        hello
+      </div>
+
+    </>
+  )
+}
+
+export default App
